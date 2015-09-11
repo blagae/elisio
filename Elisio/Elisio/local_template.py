392b1bd16662319beca392cb25964f6099522e0d
blagae <benoit.lagae@itextpdf.com>
2015-09-11 03:54:53 +0200

    Add template file to create local.py easily

---
+++ Elisio/Elisio/local_template.py
@@ -0,0 +1,10 @@
+# copy this file to Elisio/local.py
+# and change database settings at will
+DATABASE_SETTINGS = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydb.db'
+        }
+    }
+ADMIN_LIST = {}
+DEBUG_SETTING = True
