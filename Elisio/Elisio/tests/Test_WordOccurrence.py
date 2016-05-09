8bc79374863c7f7211851a6aead999acd53a329b
Benoit Lagae <benoit.lagae@itextpdf.com>
2016-05-09 21:54:23 +0200

    Add tests for failing dictionary lookup for 'se'

---
+++ Elisio/Elisio/tests/Test_WordOccurrence.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+import mock
+from Elisio.engine.Word import Word
+from Elisio.engine.Verse import Weight, set_django
+
+set_django()
+
+from Elisio.models import WordOccurrence
+ 
+class Test_WordOccurrence(TestCase):
+
+    def test_se_dict_anceps(self):
+        WordOccurrence.objects.create(word = "se",struct = "3")
+        word = Word("se", True)
+        word.split()
+        weights = [Weight.ANCEPS]
+        self.assertEqual(word.get_syllable_structure(), weights)
+
+    def test_se_dict_heavy(self):
+        WordOccurrence.objects.create(word = "se",struct = "2")
+        word = Word("se", True)
+        word.split()
+        weights = [Weight.HEAVY]
+        self.assertEqual(word.get_syllable_structure(), weights)
+
+    def test_se_dict_order1(self):
+        WordOccurrence.objects.create(word = "se",struct = "2")
+        WordOccurrence.objects.create(word = "se",struct = "3")
+        word = Word("se", True)
+        word.split()
+        weights = [Weight.HEAVY]
+        self.assertEqual(word.get_syllable_structure(), weights)
+
+    def test_se_dict_order2(self):
+        WordOccurrence.objects.create(word = "se",struct = "3")
+        WordOccurrence.objects.create(word = "se",struct = "2")
+        word = Word("se", True)
+        word.split()
+        weights = [Weight.HEAVY]
+        self.assertEqual(word.get_syllable_structure(), weights)
