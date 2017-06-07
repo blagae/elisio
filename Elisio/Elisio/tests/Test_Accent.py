59f9b555e906b7963e567cb6dc13bf8b5753b636
Benoit Lagae <benoit.lagae@itextpdf.com>
2017-06-07 12:59:46 +0200

    Add Zeleny test with elisions

--- Elisio/Elisio/tests/Test_Accent.py
+++ Elisio/Elisio/tests/Test_Accent.py
@@ -54,3 +54,31 @@ class TestAccent(unittest.TestCase):
         # venit
         self.assertEqual(verse.words[4].syllables[0].stressed, True)
         self.assertEqual(verse.words[4].syllables[1].stressed, False)
+        
+    def test_accent_litora(self):
+        verse = VerseFactory.create("litora, multum ille et terris iactatus et alto", classes=HexameterCreator)
+        verse.parse()
+        # litora
+        self.assertEqual(verse.words[0].syllables[0].stressed, True)
+        self.assertEqual(verse.words[0].syllables[1].stressed, False)
+        self.assertEqual(verse.words[0].syllables[2].stressed, False)
+        # multum
+        self.assertEqual(verse.words[1].syllables[0].stressed, True)
+        self.assertEqual(verse.words[1].syllables[1].stressed, False)
+        # ille
+        self.assertEqual(verse.words[2].syllables[0].stressed, True)
+        self.assertEqual(verse.words[2].syllables[1].stressed, False)
+        # et
+        self.assertEqual(verse.words[3].syllables[0].stressed, True)
+        # terris
+        self.assertEqual(verse.words[4].syllables[0].stressed, True)
+        self.assertEqual(verse.words[4].syllables[1].stressed, False)
+        # iactatus
+        self.assertEqual(verse.words[5].syllables[0].stressed, False)
+        self.assertEqual(verse.words[5].syllables[1].stressed, True)
+        self.assertEqual(verse.words[5].syllables[2].stressed, False)
+        # et
+        self.assertEqual(verse.words[6].syllables[0].stressed, True)
+        # alto
+        self.assertEqual(verse.words[7].syllables[0].stressed, True)
+        self.assertEqual(verse.words[7].syllables[1].stressed, False)
--- Elisio/Elisio/tests/Test_Zeleny.py
+++ Elisio/Elisio/tests/Test_Zeleny.py
@@ -8,12 +8,16 @@ class TestAccent(unittest.TestCase):
         verse = VerseFactory.create(TYPICAL_VERSE, classes=HexameterCreator)
         verse.parse()
         correct = [4,3,3,4,2,3,1,4]
-        score = verse.get_zeleny_score()
-        self.assertEqual(correct, score)
+        self.assertEqual(correct, verse.get_zeleny_score())
 
     def test_zeleny_lavinia(self):
         verse = VerseFactory.create("Italiam fato profugus Laviniaque venit", classes=HexameterCreator)
         verse.parse()
         correct = [2,4,4,6,4,4]
-        score = verse.get_zeleny_score()
-        self.assertEqual(correct, score)
+        self.assertEqual(correct, verse.get_zeleny_score())
+    
+    def test_zeleny_litora(self):
+        verse = VerseFactory.create("litora, multum ille et terris iactatus et alto", classes=HexameterCreator)
+        verse.parse()
+        correct = [4,2,2,2,6,3,1,4]
+        self.assertEqual(correct, verse.get_zeleny_score())
