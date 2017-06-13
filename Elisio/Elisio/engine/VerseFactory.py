b12a0540248ab8e5291c1460f4d108716e29900b
Benoit Lagae <benoit.lagae@itextpdf.com>
2017-06-13 17:11:03 +0200

    Make VerseFactoryImpl a nested class for encaps

--- Elisio/Elisio/engine/VerseFactory.py
+++ Elisio/Elisio/engine/VerseFactory.py
@@ -35,111 +35,112 @@ class VerseForm(enum.Enum):
         return [VerseType.UNKNOWN]
 
 class VerseFactory(object):
+    def something(self):
+        pass
+    class VerseFactoryImpl(object):
+        def __init__(self, verse, useDict=False, classes=None):
+            self.verse = verse
+            self.use_dict = useDict
+            self.words = []
+            self.layers = [[]]
+            self.flat_list = []
+            # https://docs.python.org/3/tutorial/controlflow.html#default-argument-values
+            if isinstance(classes, VerseType):
+                self.classes = classes.get_creators()
+            elif isinstance(classes, collections.Iterable):
+                self.classes = set()
+                for clazz in classes:
+                    self.classes.update(clazz.get_creators())
+            else:
+                self.classes = VerseType.UNKNOWN.get_creators()
+
+        def is_from_db(self):
+            from Elisio.models import DatabaseVerse
+            return isinstance(self.verse, DatabaseVerse)
+
+        def get_text(self):
+            if self.is_from_db():
+                return self.verse.contents
+            return self.verse
+
+        def split(self):
+            """ Split a Verse into Words, remembering only the letter characters """
+            txt = self.get_text().strip()
+            array = re.split('[^a-zA-Z]+', txt)
+            for word in array:
+                if word.isalpha():
+                    self.words.append(Word(word, self.use_dict))
+            return self.words
+
+        def layer(self):
+            """ get available weights of syllables """
+            self.split()
+            result = []
+            for count, word in enumerate(self.words):
+                try:
+                    syll_struct = word.get_syllable_structure(self.words[count+1])
+                except IndexError:
+                    syll_struct = word.get_syllable_structure()
+                result.append(syll_struct)
+            self.layers = result
+            return self.layers
+
+        def getlist(self):
+            self.layer()
+            for word in self.layers:
+                if len(word) == 1 and word[0] == Weight.ANCEPS:
+                    word[0] == Weight.HEAVY
+                for weight in word:
+                    if weight != Weight.NONE:
+                        self.flat_list.append(weight)
+            return self.flat_list
+
+        def create(self):
+            self.getlist()
+            problems = []
+            for creator in self.classes:
+                item = creator(self.flat_list)
+                cls = item.get_subtype()
+                verse = cls(self.get_text())
+                verse.words = self.words
+                verse.flat_list = self.flat_list.copy()
+                try:
+                    if self.is_from_db():
+                        verse.parse(self.verse)
+                    else:
+                        verse.parse()
+                    return verse
+                except ScansionException as exc:
+                    problems.append(exc)
+            raise VerseException("parsing did not succeed", problems)
+
+        def get_split_syllables(self):
+            result = ""
+            if not self.words:
+                self.layer()
+            for word in self.words:
+                for syll in word.syllables:
+                    for snd in syll.sounds:
+                        for ltr in snd.letters:
+                            result += ltr.letter
+                    result += "-"
+                result = result[:-1] + " "
+            return result
     @classmethod
     def split(cls, text):
-        return VerseFactoryImpl(text).split()
+        return VerseFactory.VerseFactoryImpl(text).split()
     @classmethod
     def layer(cls, text):
-        return VerseFactoryImpl(text).layer()
+        return VerseFactory.VerseFactoryImpl(text).layer()
     @classmethod
     def getlist(cls, text):
-        return VerseFactoryImpl(text).getlist()
+        return VerseFactory.VerseFactoryImpl(text).getlist()
     @classmethod
     def get_split_syllables(cls, text):
-        return VerseFactoryImpl(text).get_split_syllables()
+        return VerseFactory.VerseFactoryImpl(text).get_split_syllables()
     @classmethod
     def create(cls, verse, useDict=False, classes=None):
-        return VerseFactoryImpl(verse, useDict, classes=classes).create()
-
-class VerseFactoryImpl(object):
-    def __init__(self, verse, useDict=False, classes=None):
-        self.verse = verse
-        self.use_dict = useDict
-        self.words = []
-        self.layers = [[]]
-        self.flat_list = []
-        # https://docs.python.org/3/tutorial/controlflow.html#default-argument-values
-        if isinstance(classes, VerseType):
-            self.classes = classes.get_creators()
-        elif isinstance(classes, collections.Iterable):
-            self.classes = set()
-            for clazz in classes:
-                self.classes.update(clazz.get_creators())
-        else:
-            self.classes = VerseType.UNKNOWN.get_creators()
-
-    def is_from_db(self):
-        from Elisio.models import DatabaseVerse
-        return isinstance(self.verse, DatabaseVerse)
-
-    def get_text(self):
-        if self.is_from_db():
-            return self.verse.contents
-        return self.verse
-
-    def split(self):
-        """ Split a Verse into Words, remembering only the letter characters """
-        txt = self.get_text().strip()
-        array = re.split('[^a-zA-Z]+', txt)
-        for word in array:
-            if word.isalpha():
-                self.words.append(Word(word, self.use_dict))
-        return self.words
-
-    def layer(self):
-        """ get available weights of syllables """
-        self.split()
-        result = []
-        for count, word in enumerate(self.words):
-            try:
-                syll_struct = word.get_syllable_structure(self.words[count+1])
-            except IndexError:
-                syll_struct = word.get_syllable_structure()
-            result.append(syll_struct)
-        self.layers = result
-        return self.layers
-
-    def getlist(self):
-        self.layer()
-        for word in self.layers:
-            if len(word) == 1 and word[0] == Weight.ANCEPS:
-                word[0] == Weight.HEAVY
-            for weight in word:
-                if weight != Weight.NONE:
-                    self.flat_list.append(weight)
-        return self.flat_list
-
-    def create(self):
-        self.getlist()
-        problems = []
-        for creator in self.classes:
-            item = creator(self.flat_list)
-            cls = item.get_subtype()
-            verse = cls(self.get_text())
-            verse.words = self.words
-            verse.flat_list = self.flat_list.copy()
-            try:
-                if self.is_from_db():
-                    verse.parse(self.verse)
-                else:
-                    verse.parse()
-                return verse
-            except ScansionException as exc:
-                problems.append(exc)
-        raise VerseException("parsing did not succeed", problems)
-
-    def get_split_syllables(self):
-        result = ""
-        if not self.words:
-            self.layer()
-        for word in self.words:
-            for syll in word.syllables:
-                for snd in syll.sounds:
-                    for ltr in snd.letters:
-                        result += ltr.letter
-                result += "-"
-            result = result[:-1] + " "
-        return result
+        return VerseFactory.VerseFactoryImpl(verse, useDict, classes).create()
 
 class VerseCreator(object):
     def get_type(self):
