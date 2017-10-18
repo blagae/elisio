8a51c91a86771c7547e370991dabe2611cae0548
Benoit Lagae <benoit.lagae@itextpdf.com>
2017-10-18 23:28:34 +0200

    Add CSRF token to header by default (all js files)

--- Elisio/Elisio/static/batchScripts.js
+++ Elisio/Elisio/static/batchScripts.js
@@ -1,23 +1,4 @@
-﻿function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        var c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            var c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) c_end = document.cookie.length;
-            return decodeURI(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-}
-
-$(function () {
-    $.ajaxSetup({
-        headers: { "X-CSRFToken": getCookie("csrftoken") }
-    });
-});
-
-function resetBatchField(objects) {
+﻿function resetBatchField(objects) {
     objects.empty();
     objects.append($("<option />").val("All").text("All"));
     return objects;
