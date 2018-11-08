9f9996f76dc97c6d8db0dce54f1f7e0f47733f84
Benoit Lagae <benoit.lagae@itextpdf.com>
2018-11-08 01:29:28 +0100

    Finally adapt to updated open_words package & code

--- Elisio/Elisio/tests/unit/Test_OpenWords.py
+++ Elisio/Elisio/tests/unit/Test_OpenWords.py
@@ -1,8 +1,9 @@
 import unittest
-from open_words.parse import Parse
+from open_words.parse import Parser
 
 
 class TestOpenWords(unittest.TestCase):
+
     def test_basic_itr(self):
-        parse = Parse()
+        parse = Parser()
         print(parse.parse("volvit"))
